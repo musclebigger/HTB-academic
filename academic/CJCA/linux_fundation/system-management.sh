6ee1d74ba613f4dcc648161dc67a5899b9dfeb87
musclebigger <44393544+musclebigger@users.noreply.github.com>
2025-12-30 12:14:05 +0800

    网络服务基础和文件共享

--- academic/CJCA/linux_fundation/system-management.sh
+++ academic/CJCA/linux_fundation/system-management.sh
@@ -30,3 +30,27 @@ kill -20 PID # 恢复暂停的进程
 bg # 将暂停的进程放到后台运行，如果命令又&符号则直接后台运行
 fg # 将后台进程放到前台运行，使用jobs查看后台任务序号
 jobs # 显示当前用户的后台任务，显示序号
+
+# 定时服务创建
+sudo mkdir /etc/systemd/system/mytimer.timer.d
+sudo vim /etc/systemd/system/mytimer.timer
+# 重启守护进程
+sudo systemctl daemon-reload
+
+# crond
+# 在第 0 分钟（整点）。*每一天,每一周，每月
+0 */6 * * * /path/to/update_software.sh # 每6小时执行一次脚本,/ 表示“步长”（Step）。意思是只要小时数能被 6 整除就执行（0, 6, 12, 18）。
+0 0 1 * * /path/to/scripts/run_scripts.sh # 每月1号的0点执行脚本
+
+
+# NFS share
+mkdir nfs_sharing
+echo '/home/cry0l1t3/nfs_sharing hostname(rw,sync,no_root_squash)' >> /etc/exports
+# mount NFS share
+mkdir ~/target_nfs # 创建挂载点
+mount 10.129.12.17:/home/john/dev_scripts ~/target_nfs # 挂载NFS共享目录
+# web服务通过nodejs
+sudo npm install --global http-server
+http-server -p 8080 # 在当前目录启动web服务，监听8080端口
+# php启动web服务
+php -S 127.0.0.1:8080
